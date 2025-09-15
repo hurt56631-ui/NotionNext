c68647458ceb7ca2226b04a680d3a670b6ac7619
Hurtg185 <91710543+Hurtg185@users.noreply.github.com>
2025-09-16 04:34:17 +0630

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -1,8 +1,11 @@
+// themes/heo/components/Footer.js 【修改版】
+
 import { BeiAnGongAn } from '@/components/BeiAnGongAn'
 import CopyRightDate from '@/components/CopyRightDate'
 import PoweredBy from '@/components/PoweredBy'
 import { siteConfig } from '@/lib/config'
-import SocialButton from './SocialButton'
+// import SocialButton from './SocialButton' // 1. 不再需要导入 SocialButton
+
 /**
  * 页脚
  * @returns
@@ -12,25 +15,27 @@ const Footer = () => {
   const BEI_AN_LINK = siteConfig('BEI_AN_LINK')
   const BIO = siteConfig('BIO')
   return (
-    <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
+    <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm'>
       {/* 颜色过度区 */}
       <div
         id='color-transition'
-        className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
+        className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
       />
 
-      {/* 社交按钮 */}
-      <div className='w-full h-24'>
-        <SocialButton />
-      </div>
-
-      <br />
-
+      {/* 2. 删除了社交按钮及其容器 div */}
+      {/* 
+        <div className='w-full h-24'>
+          <SocialButton />
+        </div>
+        <br />
+      */}
+      
       {/* 底部页面信息 */}
       <div
         id='footer-bottom'
-        className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
-        <div id='footer-bottom-left' className='text-center lg:text-start'>
+        // 3. 移除了顶部间距，让它更紧凑
+        className='w-full h-auto flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
+        <div id='footer-bottom-left' className='text-center lg:text-start py-4 lg:py-0'>
           <PoweredBy />
           <div className='flex gap-x-1'>
             <CopyRightDate />
@@ -43,7 +48,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div id='footer-bottom-right'>
+        <div id='footer-bottom-right' className='text-center'>
           {BEI_AN && (
             <>
               <i className='fas fa-shield-alt' />{' '}
@@ -62,8 +67,6 @@ const Footer = () => {
             <i className='fas fa-users' />{' '}
             <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
           </span>
-
-          {/* <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1> */}
         </div>
       </div>
     </footer>
