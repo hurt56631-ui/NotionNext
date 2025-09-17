bc1be1ed8b6be27e20c753bd86c1cb4f7bf7e9e4
Hurtg185 <91710543+Hurtg185@users.noreply.github.com>
2025-09-18 05:16:39 +0630

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,23 @@
 // _app.js (最终清理版)
 
-// 导入你的样式文件...
-//import '@/styles/animate.css'
 import '@/styles/globals.css'
 import '@/styles/utility-patterns.css'
 import '@/styles/notion.css'
 import 'react-notion-x/src/styles.css'
-
-// 导入 Firebase Auth Provider
 import { AuthProvider } from '../lib/AuthContext'
 // 不再需要 DrawerProvider
-// import { DrawerProvider } from '../lib/DrawerContext' 
-
 import useAdjustStyle from '@/hooks/useAdjustStyle'
 import { GlobalContextProvider } from '@/lib/global'
 import { getBaseLayoutByTheme } from '@/themes/theme'
 import { useRouter } from 'next/router'
 import { useCallback, useMemo } from 'react'
 import { getQueryParam } from '../lib/utils'
-
 import BLOG from '@/blog.config'
 import ExternalPlugins from '@/components/ExternalPlugins'
 import SEO from '@/components/SEO'
 
-/**
- * App挂载DOM 入口文件
- */
 const MyApp = ({ Component, pageProps }) => {
   useAdjustStyle()
-
   const route = useRouter()
   const theme = useMemo(() => {
     return (
@@ -56,7 +45,6 @@ const MyApp = ({ Component, pageProps }) => {
     </GlobalContextProvider>
   )
   
-  // 回归原始，只保留 AuthProvider，不再需要 DrawerProvider
   return (
     <AuthProvider>
       {content}
