0e7a788d7e1b4cd994873a3647ea133fecd4ee32
hurt56631-ui <hurt56631@gmail.com>
2025-10-03 12:35:28 +0630

    动态海洋背景Create OceanBackground.js

---
+++ components/OceanBackground.js
@@ -0,0 +1,14 @@
+// components/bottle/OceanBackground.js
+import React from 'react';
+
+// 这个组件使用纯CSS来创建动态的波浪效果
+const OceanBackground = () => {
+  return (
+    <div className="ocean">
+      <div className="wave"></div>
+      <div className="wave"></div>
+    </div>
+  );
+};
+
+export default OceanBackground;
