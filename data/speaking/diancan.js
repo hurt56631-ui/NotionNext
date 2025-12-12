2284e430553fa0f79b3180871c060acdd36ad134
hurt56631-ui <hurt56631@gmail.com>
2025-12-12 22:05:12 +0630

    Create diancan.js口语点餐

---
+++ data/speaking/diancan.js
@@ -0,0 +1,4 @@
+export default [
+  { id: 7, chinese: '服务员，点餐。', burmese: 'စားပွဲထိုး၊ အော်ဒါမှာမယ်။', xieyin: '萨布欸多，order 嘛咩' },
+  { id: 8, chinese: '这个菜辣吗？', burmese: 'ဒီဟင်းက စပ်သလား?', xieyin: '迪 henn 嘎 萨德拉' }
+];
