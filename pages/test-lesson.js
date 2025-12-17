af7ff0032773a25b38af7872d95137bda70c96fd
hurt56631-ui <hurt56631@gmail.com>
2025-12-17 07:52:01 +0630

    测试互动课

---
+++ pages/test-lesson.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+// 1. 引入你的主互动组件
+import InteractiveLesson from '../components/InteractiveLesson'; 
+// 2. 引入刚才创建的数据
+import { hskLessonData } from '../data/hsk1-lesson1'; 
+
+export default function TestLessonPage() {
+  return (
+    <>
+      <Head>
+        <title>HSK 1 - Lesson 1 Test</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+      </Head>
+      
+      {/* 渲染互动课程 */}
+      <InteractiveLesson lesson={hskLessonData} />
+    </>
+  );
+}
